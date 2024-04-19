ce897b1aa3486062d28b3b5137e4a0c7f4bbe635
LukaBRa <banovicluka10@gmail.com>
2024-04-19 12:38:58 +0200

    Added api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://hvcmrvaccg.execute-api.eu-north-1.amazonaws.com/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
